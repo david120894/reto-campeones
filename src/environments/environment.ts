2e68a809cb7827947b432e384297f9f90aa6d938
= <consachapi@gmail.com>
2025-04-15 22:12:04 -0500

    api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     appVersion: 'v1.0.0',
     appRealm: 'municusco',
     appClientId: '',
-    apiUrl: 'http://127.0.0.1:8000/api',
+    apiUrl: 'https://api-convenciones.cusco.gob.pe/api'
 };
