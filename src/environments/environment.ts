a7d6e7940f056abd2a6716e15ac494ac1df60d64
David Jesus <jesus251293@gmail.com>
2025-05-16 08:54:56 -0500

    'refactoring'

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   appVersion: '1.0.6',
   appRealm: 'municusco',
   appClientId: '',
-  apiUrl: 'https://hatunphaway.cusco.gob.pe/api',
-  // apiUrl: 'http://172.16.4.74:30011/api',
+  // apiUrl: 'https://hatunphaway.cusco.gob.pe/api',
+  apiUrl: 'http://172.16.4.74:30011/api',
 };
