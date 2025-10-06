27ce5d3df0d8f7345c49cd8b7accdf72af00391e
David Jesus <jesus251293@gmail.com>
2025-10-06 16:39:40 -0500

    cambio de tipo de evento de el boton de enviar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,6 @@ export const environment = {
   appClientId: '',
   // apiUrl: 'https://hatunphaway.cusco.gob.pe/api',
   apiUrl: 'https://proyectocuscodeportes.cusco.gob.pe/api',
-  // apiUrl: location.origin+'/api',
+  // apiUrl: 'http://172.16.4.76:30014/api',
+  // apiUrl: location.origin+'/api',http://localhost:4200/user-register
 };
