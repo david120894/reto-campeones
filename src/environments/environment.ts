8aeb9fd55113b972c4f76284fd192f2dea1100ec
David Jesus <jesus251293@gmail.com>
2025-10-07 09:47:49 -0500

    resolved conflict

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   appRealm: 'municusco',
   appClientId: '',
   // apiUrl: 'https://hatunphaway.cusco.gob.pe/api',
-  // apiUrl: 'https://proyectocuscodeportes.cusco.gob.pe/api',
-  apiUrl: 'http://172.16.4.76:30014/api',
+  apiUrl: 'https://proyectocuscodeportes.cusco.gob.pe/api',
+  // apiUrl: 'http://172.16.4.76:30014/api',
   // apiUrl: location.origin+'/api',http://localhost:4200/user-register
 };
